b464bbba7f4411b55376bf52b2e20fe74d26bbaa
Jay Smith <jay@jaysmith.dev>
2025-07-20 23:59:30 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "https://crawlee.dev/js/docs/next/quick-start",
+  match: "https://crawlee.dev/js/docs/next/**",
+  maxPagesToCrawl: 500,
+  outputFileName: "output/crawlee.json",
   maxTokens: 2000000,
 };
